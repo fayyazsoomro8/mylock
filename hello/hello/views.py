355e9e2bf4e5aba0b88f15d1527a67a8f355589b
fayayz <fayyazsoomro01@gmail.com>
2024-06-30 07:39:23 +0500

    This is second commit

---
+++ hello/hello/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request,'index.html')
+
+def home(request):
+    return render(request,'home.html')
+
+
